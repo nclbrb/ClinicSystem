d37b45a3eef342f2fe4563d74107575ae9139a95
Nicole Borabo <borabonicole19@gmail.com>
2025-02-06 20:38:59 +0800

    Sidebar's Contents

--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,39 @@
 </head>
 <body>
     <nav class="sidebar">
+        <header>
+        </header>
+        <div class="menu-bar">
+            <div class="menu">
+                <li class="search-box">
+                    <input type="text" placeholder="Search...">
+                </li>
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="">
+                            <span class="text nav-text">Homepage</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <span class="text nav-text">Admit a Patient</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="#">
+                            <span class="text nav-text">Patient List</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="bottom-content">
+                <li class="">
+                    <a href="login.php">
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+            </div>
+        </div>
     </nav>
 </body>
 </html>
