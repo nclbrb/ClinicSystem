7f062c82e380777cb6d95dafece34f9374faedab
Blanco <blancojohcedric89@gmail.com>
2025-02-09 20:38:45 +0800

    patient admission form add css style

--- patient_form.php
+++ patient_form.php
@@ -55,7 +55,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="trys.css">
+    <link rel="stylesheet" href="patient_form.css">
     <title>Document</title>
     <script>
         function validateForm(event) {
