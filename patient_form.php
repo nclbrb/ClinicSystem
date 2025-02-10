edfe28d5803f9ca998cbd6ad751195a87c32bf2e
Blanco <blancojohcedric89@gmail.com>
2025-02-10 10:10:26 +0800

    additional changes

--- patient_form.php
+++ patient_form.php
@@ -54,7 +54,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="qwe.css">
+    <link rel="stylesheet" href="patient_form.css">
     <title>Document</title>
     <script>
        function validateForm(event) {
